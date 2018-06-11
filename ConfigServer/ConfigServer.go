a6660835496b135ccd4b2ae43a10aa3f046b8f88
Jose <jose@localhost.localdomain>
2018-06-11 14:46:17 -0500

    Cache en config server

--- ConfigServer/ConfigServer.go
+++ ConfigServer/ConfigServer.go
@@ -6,14 +6,18 @@ import (
 	"net/http"
 	"os/exec"
 	"github.com/gorilla/mux"
+	"github.com/patrickmn/go-cache"
+	"time"
 )
 
 type API struct {
 	Key string `json:"key"`
 }
 
-func main()  {
+var c = cache.New(5*time.Minute, 10*time.Minute)
 
+func main()  {
+	
 	ruta := mux.NewRouter()
 	ruta.HandleFunc("/key", GetKey).Methods("GET")
 
@@ -24,20 +28,36 @@ func main()  {
 func GetKey(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	enableCors(&w)
-	cmd := "gcloud auth print-access-token"
-	out, err := exec.Command("sh","-c",cmd).Output()
 
-	if err != nil {
-		fmt.Println("Error")
-	} else {
-		str := fmt.Sprintf("%s", out[:len(out) - 1])
-		fmt.Print(str)
-		message :=API{ Key : str } 
-		json.NewEncoder(w).Encode(message)
-	}
+	message :=API{ Key : generateKey() }
+	json.NewEncoder(w).Encode(message)
 
 }
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+func generateKey() string {
+
+	foo, found := c.Get("data")
+	if found {
+		fmt.Println("-----Found in Cache-----")
+		fmt.Println(foo)
+		fmt.Println("--------------")
+		return foo.(string)
+	}else {
+		cmd := "gcloud auth print-access-token"
+		out, err := exec.Command("sh","-c",cmd).Output()
+
+		if err != nil {
+			fmt.Println("Error")
+		} else {
+			str := fmt.Sprintf("%s", out[:len(out) - 1])
+			c.Set("data", str, cache.DefaultExpiration)
+			return str
+		}
+		return ""
+	}
+
 }
